94eb1cedb08dfaeb75a815ef5b5f948619e19028
arslan-cyberTITU <rasulov.titu@gmail.com>
2021-12-01 17:11:04 +0500

    message

--- api/mobile/main.py
+++ api/mobile/main.py
@@ -26,4 +26,4 @@ app.include_router(constant_router,       tags=["Constant"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, debug=False)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True, debug=False)
